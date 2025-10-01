ec9387a0e032d875091ef6d9ae8b78b94a7cb2b6
oliviahe0111 <oliviahe0111@gmail.com>
2025-10-01 16:03:23 -0700

    Fix basePath for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/Portfolio' : '',
 };
 
 export default nextConfig;
